4f67ec53ab311da0f0d31c441c1fedb5278a9a22
amira-bahey <133486868+amira-bahey@users.noreply.github.com>
2024-08-28 16:44:26 +0300

    Add files via upload

---
+++ Backend/dft/dft_script.tcl
@@ -0,0 +1,162 @@
+
+########################### Define Top Module ############################
+                                                   
+set top_module system_top_dft
+
+##################### Define Working Library Directory ######################
+                                                   
+define_design_lib work -path ./work
+
+########################### Formality Setup file ############################
+
+set_svf system.svf
+
+################## Design Compiler Library Files #setup ######################
+
+lappend search_path /home/IC/Projects/std_cells
+lappend search_path /home/IC/Projects/rtl
+
+set SSLIB "scmetro_tsmc_cl013g_rvt_ss_1p08v_125c.db"
+set TTLIB "scmetro_tsmc_cl013g_rvt_tt_1p2v_25c.db"
+set FFLIB "scmetro_tsmc_cl013g_rvt_ff_1p32v_m40c.db"
+
+## Standard Cell libraries 
+set target_library [list $SSLIB $TTLIB $FFLIB]
+
+## Standard Cell & Hard Macros libraries 
+set link_library [list * $SSLIB $TTLIB $FFLIB]  
+
+#echo "###############################################"
+#echo "############# Reading RTL Files  ##############"
+#echo "###############################################"
+
+analyze -format verilog [glob /home/IC/Projects/rtl/*.v]
+analyze -format sverilog [glob /home/IC/Projects/rtl/*.sv]
+elaborate -lib work $top_module
+
+###################### Defining toplevel ###################################
+
+current_design $top_module
+
+#################### Liniking All The Design Parts #########################
+puts "###############################################"
+puts "######## Liniking All The Design Parts ########"
+puts "###############################################"
+
+link 
+
+#################### Liniking All The Design Parts #########################
+puts "###############################################"
+puts "######## checking design consistency ##########"
+puts "###############################################"
+
+check_design
+
+############################### Path groups ################################
+puts "###############################################"
+puts "################ Path groups ##################"
+puts "###############################################"
+
+group_path -name INREG -from [all_inputs]
+group_path -name REGOUT -to [all_outputs]
+group_path -name INOUT -from [all_inputs] -to [all_outputs]
+
+#################### Define Design Constraints #########################
+puts "###############################################"
+puts "############ Design Constraints #### ##########"
+puts "###############################################"
+
+source -echo ./cons.tcl
+
+#################### Archirecture Scan Chains #########################
+puts "###############################################"
+puts "############ Configure scan chains ############"
+puts "###############################################"
+
+set_scan_configuration -clock_mixing no_mix  -style multiplexed_flip_flop -replace true -max_length 100  
+
+###################### Mapping and optimization ########################
+puts "###############################################"
+puts "########## Mapping & Optimization #############"
+puts "###############################################"
+
+
+compile -scan
+
+################################################################### 
+# Setting Test Timing Variables
+################################################################### 
+
+# Preclock Measure Protocol (default protocol)
+set test_default_period 100
+set test_default_delay 0
+set test_default_bidir_delay 0
+set test_default_strobe 20
+set test_default_strobe_width 0
+
+########################## Define DFT Signals ##########################
+
+set_dft_signal -port [get_ports scan_clk]  -type ScanClock   -view existing_dft  -timing {30 60}
+set_dft_signal -port [get_ports scan_rst]  -type Reset       -view existing_dft  -active_state 0
+set_dft_signal -port [get_ports test_mode] -type Constant    -view existing_dft  -active_state 1 
+set_dft_signal -port [get_ports test_mode] -type TestMode    -view spec          -active_state 1 
+set_dft_signal -port [get_ports SE]        -type ScanEnable  -view spec          -active_state 1   -usage scan
+set_dft_signal -port [get_ports SI]        -type ScanDataIn  -view spec 
+set_dft_signal -port [get_ports SO]        -type ScanDataOut -view spec  
+
+############################# Create Test Protocol #####################
+
+create_test_protocol
+                            
+###################### Pre-DFT Design Rule Checking ####################
+
+dft_drc -verbose
+
+############################# Preview DFT ##############################
+
+preview_dft -show scan_summary
+
+############################# Insert DFT ###############################
+
+insert_dft
+
+######################## Optimize Logic post DFT #######################
+
+compile -scan -incremental
+
+###################### Design Rule Checking post DFT ###################
+
+dft_drc -verbose -coverage_estimate
+
+##################### Close Formality Setup file ###########################
+
+set_svf -off
+
+#############################################################################
+# Write out Design after initial compile
+#############################################################################
+
+write_file -format verilog -hierarchy -output system.v
+write_file -format ddc -hierarchy -output system.ddc
+write_sdc  -nosplit system.sdc
+write_sdf           system.sdf
+################# reporting #######################
+report_power -hierarchy > power.rpt
+report_area -hierarchy > area.rpt
+report_timing -max_paths 100 -delay_type max > setup.rpt
+report_timing -max_paths 100 -delay_type min > hold.rpt
+report_clock -attributes > clocks.rpt
+report_constraint -all_violators > constraints.rpt
+
+############################################################################
+# DFT Preparation Section
+############################################################################
+
+set flops_per_chain 100
+
+set num_flops [sizeof_collection [all_registers -edge_triggered]]
+
+set num_chains [expr $num_flops / $flops_per_chain + 1 ]
+################# starting graphical user interface #######################
+
+gui_start
